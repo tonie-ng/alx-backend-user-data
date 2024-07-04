570a709cf1c7004c22be998468ee208e36dfb14e
Tonie Victor <victornwanochi@gmail.com>
2024-07-04 23:18:00 +0000

    finally...

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,20 +37,14 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Creates a connector to a database.
-    """
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
-    )
-    return connection
+    """Returns a connector to the database"""
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME") or "root"
+    paswrd = os.getenv("PERSONAL_DATA_DB_PASSWORD") or ""
+    host = os.getenv("PERSONAL_DATA_DB_HOST") or "localhost"
+    db = os.getenv("PERSONAL_DATA_DB_NAME")
+    conn = mysql.connector.connect(user=user, password=paswrd,
+                                   host=host, database=db, port=3306)
+    return conn
 
 
 def main():
